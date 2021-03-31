c342e0c3b938dcd9b8b36395e58fa7b05e9498f8
Anthony A. Hernandez-Wanhlung <TeitoWolf@hotmail.com>
2021-03-31 12:14:48 -0400

    just gettin started (init commit)

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+   // allow the user to give us upperBound, lowerBound (think validate)
+   // allow the user to set condition for multiple 7, 11
+   // count from 0 to 100, and print eaach number per line
+   // if number is multiple of 3, print fizz
+   // if number is multiple of 5, print buzz
+   // if number is multiple of both, print fizzbuzz
+   class FizzProgram
+   {
+      static void Main(string[] args)
+      {
+         FizzProgram pgm = new FizzProgram();
+         pgm.countNumbers(0, 100);
+
+      }
+      private void CheckFizzBuzz(int a, int div1, int div2)
+      {
+         if (a != 0)
+         {
+            if (a % div1 == 0 && a % div2 == 0)
+               WriteToConsole("fizzbuzz");
+            else
+            {
+               if (a % div1 == 0)
+                  WriteToConsole("fizz");
+               if (a % div2 == 0)
+                  WriteToConsole("buzz");
+            }
+
+         }
+      }
+      private void countNumbers(int start, int end, int inc = 1)
+      {
+         for (var i = start; i <= end; i += inc)
+         {
+            WriteToConsole(i);
+         }
+      }
+
+      private void WriteToConsole()
+      {
+         if (a == 0 && b != null)
+            Console.WriteLine(b);
+         else
+            Console.WriteLine(a);
+
+      }
+   }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Revature.Week1.HelloWorld
+{
+   class Program
+   {
+      static void Main()
+      {
+         Console.WriteLine("enter a name: ");
+
+         var input = Console.ReadLine();
+
+         //stout to print your name
+         Console.WriteLine($"Hello {input}");//string interpolation, template string, condenses into a single slot
+                                             //  Console.WriteLine("Hello " + input);//standard, slightly slower, takes 3 slots on a stack 
+      }
+      private void UiPath()
+      {
+
+      }
+   }
+}
